acd2efba1c020183dc68bf192caf40b3be0d256d
krishnapatil30 <krishnapatil0830@gmail.com>
2025-11-10 08:47:55 +0530

    Update finalpass1.java

--- finalpass1.java
+++ finalpass1.java
@@ -3,104 +3,96 @@ import java.util.*;
 class Pass1 {
     public static void main(String args[]) {
 
-        // Input Assembly Program
+        // Input Assembly Code
         String program[][] = {
-            {"START", "101"},
-            {"AGAIN", "MULT", "BREG", "TERM"},
-            {" ", "MOVER", "CREG", "TERM"},
-            {" ", "ADD", "CREG", "N"},
-            {" ", "MOVEM", "CREG", "TERM"},
-            {"N", "DS", "2"},
-            {"RESULT", "DS", "2"},
-            {"ONE", "DC", "1"},
-            {"TERM", "DC", "1"},
-            {"END"}
+            {"START", "100"},
+            {"AGAIN", "MOVER", "AREG", "=5"},
+            {" ", "ADD", "BREG", "N"},
+            {" ", "MOVEM", "AREG", "RESULT"},
+            {"N", "DS", "1"},
+            {"RESULT", "DS", "1"},
+            {" ", "END"}
         };
-
-        // Mnemonic Table (MOT)
-        Map<String, String> MOT = new HashMap<>();
-        MOT.put("MULT", "IS,03");
-        MOT.put("MOVER", "IS,04");
-        MOT.put("ADD", "IS,01");
-        MOT.put("MOVEM", "IS,05");
-        MOT.put("START", "AD,01");
-        MOT.put("END", "AD,02");
-        MOT.put("DC", "DL,01");
-        MOT.put("DS", "DL,02");
-
-        // Register Table
-        Map<String, String> REG = new HashMap<>();
-        REG.put("AREG", "1");
-        REG.put("BREG", "2");
-        REG.put("CREG", "3");
-        REG.put("DREG", "4");
 
         // Symbol Table
         Map<String, Integer> SYMTAB = new LinkedHashMap<>();
-
-        // Literal Table (Empty because no literals exist)
+        // Literal Table
         List<String> LITTAB = new ArrayList<>();
-
-        // Intermediate Code list
-        List<String> IC = new ArrayList<>();
+        // Literal Address Table
+        List<Integer> LITADDR = new ArrayList<>();
+        // Pool Table
+        List<Integer> POOLTAB = new ArrayList<>();
+        POOLTAB.add(0); // First pool always starts at index 0
 
         int LC = 0;
 
-        // START directive handling
+        System.out.println("\nPASS 1 PROCESSING...\n");
+
+        // START
         if (program[0][0].equals("START")) {
             LC = Integer.parseInt(program[0][1]);
-            IC.add("(AD,01) (C," + LC + ")");
+            System.out.println("LC initialized to: " + LC);
         }
 
-        // Process line by line
+        // Process instructions
         for (int i = 1; i < program.length; i++) {
 
+            // END => Assign literal addresses
             if (program[i][0].equals("END")) {
-                IC.add("(AD,02)");
+                for (int k = POOLTAB.get(POOLTAB.size() - 1); k < LITTAB.size(); k++) {
+                    LITADDR.add(LC);
+                    LC++;
+                }
+                POOLTAB.add(LITTAB.size());
                 break;
             }
 
             String label = program[i][0];
-
-            if (!label.equals(" ")) { // If label present
+            if (!label.equals(" ")) {
                 SYMTAB.put(label, LC);
             }
 
             String opcode = program[i][1];
 
             if (opcode.equals("DS")) {
-                IC.add("(DL,02) (C," + program[i][2] + ")");
                 LC += Integer.parseInt(program[i][2]);
             }
             else if (opcode.equals("DC")) {
-                IC.add("(DL,01) (C," + program[i][2] + ")");
                 LC += 1;
             }
-            else { // Normal Instruction
-                String classOp = MOT.get(opcode);
-                String reg = REG.getOrDefault(program[i][2], "-");
+            else {
+                // Check if operand is literal
                 String operand = program[i][3];
-                IC.add("(" + classOp + ") (" + reg + ") (S," + operand + ")");
+                if (operand.startsWith("=")) {
+                    if (!LITTAB.contains(operand)) {
+                        LITTAB.add(operand);
+                    }
+                }
                 LC++;
             }
         }
 
-        // OUTPUT
+        // Print SYMTAB
         System.out.println("\n******** SYMBOL TABLE ********");
         System.out.println("Symbol\tAddress");
         for (String s : SYMTAB.keySet()) {
             System.out.println(s + "\t" + SYMTAB.get(s));
         }
 
+        // Print LITTAB
         System.out.println("\n******** LITERAL TABLE ********");
-        System.out.println("(No literals present in program)");
+        System.out.println("Literal\tAddress");
+        for (int i = 0; i < LITTAB.size(); i++) {
+            System.out.println(LITTAB.get(i) + "\t" + LITADDR.get(i));
+        }
 
-        System.out.println("\n******** INTERMEDIATE CODE ********");
-        for (String line : IC) {
-            System.out.println(line);
+        // Print POOLTAB
+        System.out.println("\n******** POOL TABLE ********");
+        System.out.println("Pool#\tStart Index");
+        for (int i = 0; i < POOLTAB.size(); i++) {
+            System.out.println((i+1) + "\t" + POOLTAB.get(i));
         }
 
         System.out.println("\nPASS 1 Completed Successfully.\n");
     }
 }
-
