99f420007dafcb09e408cf8775c556538d45564c
krishnapatil30 <krishnapatil0830@gmail.com>
2025-11-10 08:44:12 +0530

    Update finalpass1.java

--- finalpass1.java
+++ finalpass1.java
@@ -1,10 +1,9 @@
 import java.util.*;
 
 class Pass1 {
-
     public static void main(String args[]) {
 
-        // Assembly Program Input
+        // Input Assembly Program
         String program[][] = {
             {"START", "101"},
             {"AGAIN", "MULT", "BREG", "TERM"},
@@ -18,54 +17,90 @@ class Pass1 {
             {"END"}
         };
 
+        // Mnemonic Table (MOT)
+        Map<String, String> MOT = new HashMap<>();
+        MOT.put("MULT", "IS,03");
+        MOT.put("MOVER", "IS,04");
+        MOT.put("ADD", "IS,01");
+        MOT.put("MOVEM", "IS,05");
+        MOT.put("START", "AD,01");
+        MOT.put("END", "AD,02");
+        MOT.put("DC", "DL,01");
+        MOT.put("DS", "DL,02");
+
+        // Register Table
+        Map<String, String> REG = new HashMap<>();
+        REG.put("AREG", "1");
+        REG.put("BREG", "2");
+        REG.put("CREG", "3");
+        REG.put("DREG", "4");
+
         // Symbol Table
-        Map<String, Integer> symbolTable = new LinkedHashMap<>();
+        Map<String, Integer> SYMTAB = new LinkedHashMap<>();
 
-        int LC = 0;  // Location Counter
+        // Literal Table (Empty because no literals exist)
+        List<String> LITTAB = new ArrayList<>();
 
-        System.out.println("\nPASS 1 PROCESSING...\n");
+        // Intermediate Code list
+        List<String> IC = new ArrayList<>();
 
-        // START directive
-        if(program[0][0].equals("START")) {
-            LC = Integer.parseInt(program[0][1]); 
-            System.out.println("START found. LC initialized to: " + LC);
+        int LC = 0;
+
+        // START directive handling
+        if (program[0][0].equals("START")) {
+            LC = Integer.parseInt(program[0][1]);
+            IC.add("(AD,01) (C," + LC + ")");
         }
 
-        // Process instructions
-        for(int i = 1; i < program.length; i++) {
+        // Process line by line
+        for (int i = 1; i < program.length; i++) {
 
-            // If line only contains END => stop
-            if(program[i].length == 1 && program[i][0].equals("END")) {
+            if (program[i][0].equals("END")) {
+                IC.add("(AD,02)");
                 break;
             }
 
             String label = program[i][0];
 
-            // If label exists
-            if(!label.equals(" ")) {
-                symbolTable.put(label, LC);
+            if (!label.equals(" ")) { // If label present
+                SYMTAB.put(label, LC);
             }
 
-            // Check for directives & normal instructions
-            if(program[i][1].equals("DS")) {
-                LC += Integer.parseInt(program[i][2]); // allocate DS
+            String opcode = program[i][1];
+
+            if (opcode.equals("DS")) {
+                IC.add("(DL,02) (C," + program[i][2] + ")");
+                LC += Integer.parseInt(program[i][2]);
             }
-            else if(program[i][1].equals("DC")) {
-                LC += 1; // DC always takes 1 word
+            else if (opcode.equals("DC")) {
+                IC.add("(DL,01) (C," + program[i][2] + ")");
+                LC += 1;
             }
-            else {
-                LC += 1; // Normal instruction size = 1 word
+            else { // Normal Instruction
+                String classOp = MOT.get(opcode);
+                String reg = REG.getOrDefault(program[i][2], "-");
+                String operand = program[i][3];
+                IC.add("(" + classOp + ") (" + reg + ") (S," + operand + ")");
+                LC++;
             }
         }
 
-        // Print Symbol Table
+        // OUTPUT
         System.out.println("\n******** SYMBOL TABLE ********");
         System.out.println("Symbol\tAddress");
-        System.out.println("----------------------");
-        for(String key : symbolTable.keySet()) {
-            System.out.println(key + "\t" + symbolTable.get(key));
+        for (String s : SYMTAB.keySet()) {
+            System.out.println(s + "\t" + SYMTAB.get(s));
+        }
+
+        System.out.println("\n******** LITERAL TABLE ********");
+        System.out.println("(No literals present in program)");
+
+        System.out.println("\n******** INTERMEDIATE CODE ********");
+        for (String line : IC) {
+            System.out.println(line);
         }
 
         System.out.println("\nPASS 1 Completed Successfully.\n");
     }
 }
+
